9ef862cf17a585056ef91163adf06fc326afde94
Markus Dittmann <dmarku@posteo.de>
2018-01-27 00:53:06 +0100

    add player sprite, basic tank controls

--- dev_markus.js
+++ dev_markus.js
@@ -7,20 +7,49 @@ const Phaser = require('phaser-ce');
 
 import GameController from './Scripts/GameController';
 
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+const game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 var gameController = new GameController();
 
 function preload () 
 {
-    
+    game.load.spritesheet('player', 'Assets/player_01.png', 512, 512);
 }
+
+let playerSprite;
+let playerForwardSpeed = 3;
+let playerBackwardSpeed = -1.5;
+let cursorKeys;
 
 function create () 
 {
+    playerSprite = game.add.sprite(640, 360, 'player');
+    playerSprite.scale.setTo(0.1, 0.1);
+    playerSprite.anchor.setTo(0.5);
 
+    cursorKeys = game.input.keyboard.createCursorKeys();
 }
 
 function update () 
 {
+    if (cursorKeys.left.isDown) {
+        playerSprite.angle -= 5;
+    }
+
+    if (cursorKeys.right.isDown) {
+        playerSprite.angle += 5;
+    }
+
+    if (cursorKeys.up.isDown) {
+        playerSprite.position.add(
+            playerForwardSpeed * Math.cos(playerSprite.rotation),
+            playerForwardSpeed * Math.sin(playerSprite.rotation));
+    }
+
+    if (cursorKeys.down.isDown) {
+        playerSprite.position.add(
+            playerBackwardSpeed * Math.cos(playerSprite.rotation),
+            playerBackwardSpeed * Math.sin(playerSprite.rotation));
+    }
+
     gameController.Update();
 }
