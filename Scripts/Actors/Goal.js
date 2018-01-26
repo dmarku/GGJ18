f23e6c09a74be5bf0527012441b134458733a358
Stephan <stephan.schueritz@web.de>
2018-01-26 22:28:29 +0100

    Add Actor InitFiles

---
+++ Scripts/Actors/Goal.js
@@ -0,0 +1,5 @@
+
+class Goal extends Actor
+{
+
+}
---
+++ Scripts/GameController.js
@@ -0,0 +1,28 @@
+
+class GameController 
+{
+    // 0 = Init
+    // 1 = Start
+    // 2 = Loop
+    // 3 = GameOver
+    state = 0;
+
+    constructor() 
+    {
+        
+    }
+
+    //#region GameSystem
+
+    StartGame()
+    {
+
+    }
+
+    Loop()
+    {
+
+    }
+
+    //#endregion
+}
