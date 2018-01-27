7e2f4cef3f4dd9751709c0784fb2c7ab73bd6d9a
Markus Dittmann <dmarku@posteo.de>
2018-01-27 20:33:48 +0100

    copy dev_stephan.js to index.js, remove dev_markus.js

--- index.js
+++ index.js
@@ -6,21 +6,86 @@ window.Phaser = require('phaser-ce/build/custom/phaser-split');
 const Phaser = require('phaser-ce');
 
 import GameController from './Scripts/GameController';
+import InputPair from './Scripts/InputController';
+import Actor from './Scripts/Actors/Actor';
+import Player from './Scripts/Actors/Player';
+import Goal from './Scripts/Actors/Goal';
+import Enemy from './Scripts/Actors/Enemy';
 
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+const game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render});
 var gameController = new GameController();
 
-function preload () 
+// --- Controls ---
+
+// --- Functions ---
+function preload() 
 {
-    
+    game.load.image('galaxie', 'Assets/galaxy_general.png');
+    game.load.spritesheet('player', 'Assets/player_01.png', 512, 512);
+    game.load.spritesheet('goal', 'Assets/goal_01.png', 512, 512, 8);
+
+    game.load.spritesheet('enemy', 'Assets/enemy_a_01.png', 512, 512, 8);
 }
 
-function create () 
-{
+let enemyData = [
+    {x: 640, y: 360}
+];
 
+function create() 
+{
+    // --- Init Background ---
+    game.add.sprite(0, 0, 'galaxie');
+
+    // --- Init Player ---
+    // -- Sprite Setup --
+    var player_sprite = game.add.sprite(0, 0, 'player');
+
+    // -- Player Input --
+    var playerInput = new InputPair(game, Phaser.Keyboard.UP, Phaser.Keyboard.DOWN);
+
+    // -- Create Player Instance --
+    var player = new Player(player_sprite, 0.2, playerInput, game);
+    player.transform.position.setTo(100, 100);
+    gameController.RegisterPlayer(player);
+
+    // --- Init Goal ---
+    var goal_sprite = game.add.sprite(0,0, 'goal');
+    var goal = new Goal(goal_sprite, 0.2, game);
+    goal.transform.position.setTo(640, 360);
+    gameController.RegisterGoal(goal);
+
+    // --- Init Enemies ---
+    let enemies = enemyData.map(enemy => {
+        let sprite = game.add.sprite(enemy.x, enemy.y, 'enemy');
+        return new Enemy(sprite, 0.2, game);
+    });
+
+    enemies.forEach(e => gameController.RegisterEnemy(e));
+
+    // --- Start Physics ---
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 }
 
-function update () 
+let foundGoal = false;
+
+function update() 
 {
     gameController.Update();
+
+    game.physics.arcade.overlap(gameController.player.sprite, gameController.goal.sprite, () => {
+        if (!foundGoal) {
+            foundGoal = true;
+            alert('You won!');
+        }
+    }, null, this);
+}
+
+function render()
+{
+    game.debug.body(gameController.player.sprite);
+    game.debug.body(gameController.goal.sprite);
+    game.debug.body(gameController.player.scanCone);
+    for (let enemy of gameController.enemies) {
+        game.debug.body(enemy.sprite);
+    }
 }
