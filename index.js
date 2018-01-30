793de3c23e22e68e5ba6d59626e796eca05265d0
Markus Dittmann <dmarku@posteo.de>
2018-01-30 09:40:07 +0100

    import Phaser as ES6 module

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.PIXI   = require('phaser-ce/build/custom/pixi');
 window.p2     = require('phaser-ce/build/custom/p2');
 window.Phaser = require('phaser-ce/build/custom/phaser-split');
 
-const Phaser = require('phaser-ce');
+import Phaser from 'phaser-ce';
 
 import GameController from './Scripts/GameController';
 import InputPair from './Scripts/InputController';
